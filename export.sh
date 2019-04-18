f23270db2206f6a29378a9a21a921d27717e35fa
Andrew Wei <andrewscwei@gmail.com>
2019-04-18 09:44:07 -0700

    Update presets

--- export.sh
+++ export.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
 
 SRC="$HOME/Library/Developer/Xcode/UserData"
-
-# Colors.
-COLOR_PREFIX="\x1b["
-COLOR_RESET=$COLOR_PREFIX"0m"
-COLOR_BLACK=$COLOR_PREFIX"0;30m"
-COLOR_RED=$COLOR_PREFIX"0;31m"
-COLOR_GREEN=$COLOR_PREFIX"0;32m"
-COLOR_ORANGE=$COLOR_PREFIX"0;33m"
-COLOR_BLUE=$COLOR_PREFIX"0;34m"
-COLOR_PURPLE=$COLOR_PREFIX"0;35m"
-COLOR_CYAN=$COLOR_PREFIX"0;36m"
-COLOR_LIGHT_GRAY=$COLOR_PREFIX"0;37m"
+CWD=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)
+COLOR_PREFIX="\x1b["; COLOR_RESET=$COLOR_PREFIX"0m"; COLOR_BLACK=$COLOR_PREFIX"0;30m"; COLOR_RED=$COLOR_PREFIX"0;31m"; COLOR_GREEN=$COLOR_PREFIX"0;32m"; COLOR_ORANGE=$COLOR_PREFIX"0;33m"; COLOR_BLUE=$COLOR_PREFIX"0;34m"; COLOR_PURPLE=$COLOR_PREFIX"0;35m"; COLOR_CYAN=$COLOR_PREFIX"0;36m"; COLOR_LIGHT_GRAY=$COLOR_PREFIX"0;37m"
 
 function export_themes() {
-  for file in $SRC/FontAndColorThemes/*; do
+  for file in ${SRC}/FontAndColorThemes/*; do
     local f=${file##*/}
-    local o="./FontAndColorThemes/$f"
+    local o="${CWD}/FontAndColorThemes/$f"
 
     echo -e "Exporting theme ${COLOR_CYAN}$f${COLOR_RESET}..."
 
@@ -26,9 +16,9 @@ function export_themes() {
 }
 
 function export_key_bindings() {
-  for file in $SRC/KeyBindings/*; do
+  for file in ${SRC}/KeyBindings/*; do
     local f=${file##*/}
-    local o="./KeyBindings/$f"
+    local o="${CWD}/KeyBindings/$f"
 
     echo -e "Exporting key binding ${COLOR_CYAN}$f${COLOR_RESET}..."
 
